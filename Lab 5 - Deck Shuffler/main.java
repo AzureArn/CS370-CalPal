8e2ee4b61aa7c4357803c542419412d4904facf7
Adam M. Barry <adam.barry@att.net>
2025-02-19 20:27:55 -0800

    Update main.java
    Basic skeleton, rectangle visable and green, Resolution set to 1920 x1080

--- Lab 5 - Deck Shuffler/main.java
+++ Lab 5 - Deck Shuffler/main.java
@@ -8,7 +8,7 @@ import javax.swing.ImageIcon;
 
 
 
-public class Main {
+public class main {
 
     public static class CardPanel extends JPanel{
 
@@ -18,8 +18,14 @@ public class Main {
             super.paintComponent(g);
             Graphics2D g2d = (Graphics2D) g;
 
-            g2d.setColor(new Color(0,200,0));
-            g2d.draw(new Rectangle2D.Double(0,0,500, 500));
+            int startX = 20;
+            int startY = 20;
+            int width = getWidth();
+            int height = getHeight();
+
+            g2d.setColor(new Color(29, 92, 29));
+            g2d.fillRect(startX, startY, width, height);
+            g2d.draw(new Rectangle2D.Double(startX, startY, width, height));
 
         }
     }
@@ -30,12 +36,16 @@ public class Main {
         CardPanel panel = new CardPanel();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // makes the program exit the JVM on close
-        int size = 500;
+
+        int sizeX = 1920; // A standard resolution to use
+        int sizeY = 1080;
+
         panel.setLayout(null); // necessary to allow for custom positioning of button
-        frame.setSize(size, size);
-        frame.setResizable(false); // prevents changing window from 500x500 size
+        frame.setSize(sizeX, sizeY);
+        frame.setResizable(false); // prevents changing window from changing res size
+
         //button to redraw
-        JButton redrawButton = new JButton("Redraw");
+        JButton redrawButton = new JButton("Shuffle");
         redrawButton.addActionListener(new ActionListener() { //action for button
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -43,8 +53,8 @@ public class Main {
             }
         });
 
-        int locX = 0;
-        int locY = size - (size / 5);
+        int locX = (sizeX / 2);
+        int locY = (sizeY-100);
         redrawButton.setLocation(locX, locY);
         redrawButton.setSize(500, 50);
         frame.getContentPane().add(panel);
