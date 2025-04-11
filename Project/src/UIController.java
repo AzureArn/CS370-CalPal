d26f4441d5997ec8258ced873218725efbbfde48
Portalguy598 <ryandmilner@gmail.com>
2025-04-10 18:01:04 -0700

    Placed button actions in their own methods to make the class more readable, should add more methods as needed

--- Project/src/UIController.java
+++ Project/src/UIController.java
@@ -7,13 +7,13 @@ public class UIController {
     View view;
     // the loginManager and dataManager interact with the user credential data and calorie data respectively
     UserLoginManager loginManager;
-    UserDataManager dataManager;
+    UserDataManager userDataManager;
 
     public UIController(View view){
         this.view = view;
         loginManager = new UserLoginManager();
         // instantiate data manager later when a successful login occurs
-        dataManager = null;
+        userDataManager = null;
 
         // set various action listeners for components
 
@@ -21,22 +21,7 @@ public class UIController {
         view.getCreateUserButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // get the name and password from the field
-                String name = view.getUserField().getText();
-                String password = view.getPasswordField().getText();
-
-                // try to create the user using the loginManager
-                boolean success = loginManager.addUser(name, password);
-
-                if(success){
-                    // use the message label to tell the user it was successful
-                    view.getLoginMessageLabel().setText("User " + name.strip() + " added");
-                }
-                else{
-                    // use the message label to tell the user it was not successful
-                    view.getLoginMessageLabel().setText("Could not add user," +
-                            " name is either taken or name/password includes : character");
-                }
+                addUser();
             }
         });
 
@@ -44,25 +29,54 @@ public class UIController {
         view.getLoginButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // get the name and password from the field
-                String name = view.getUserField().getText();
-                String password = view.getPasswordField().getText();
-
-                // try to log in the user using the loginManager
-                boolean success = loginManager.logIn(name, password);
-
-                if(success){
-                    // swap the view
-                    view.swapView();
-                    //instantiate dataManager using the username
-                    dataManager = new UserDataManager(name.strip());
-                    view.getTestLabel().setText("User " + name.strip() + " logged in");
-                }
-                else{
-                    // use the message label to tell the user login was not successful
-                    view.getLoginMessageLabel().setText("Could not log in");
-                }
+                logIn();
             }
         });
+    }
+
+    // create methods for each button / other component that sends action here
+
+    // createUserButton's function
+    // adds a new user
+    public void addUser(){
+        // get the name and password from the field
+        String name = view.getUserField().getText();
+        String password = view.getPasswordField().getText();
+
+        // try to create the user using the loginManager
+        boolean success = loginManager.addUser(name, password);
+
+        if(success){
+            // use the message label to tell the user it was successful
+            view.getLoginMessageLabel().setText("User " + name.strip() + " added");
+        }
+        else{
+            // use the message label to tell the user it was not successful
+            view.getLoginMessageLabel().setText("Could not add user," +
+                    " name is either taken or name/password includes : character");
+        }
+    }
+
+    // loginButton's function
+    // attempts to log user in, switches screen if successful
+    public void logIn(){
+        // get the name and password from the field
+        String name = view.getUserField().getText();
+        String password = view.getPasswordField().getText();
+
+        // try to log in the user using the loginManager
+        boolean success = loginManager.logIn(name, password);
+
+        if(success){
+            // swap the view
+            view.swapView();
+            //instantiate dataManager using the username
+            userDataManager = new UserDataManager(name.strip());
+            view.getTestLabel().setText("User " + name.strip() + " logged in");
+        }
+        else{
+            // use the message label to tell the user login was not successful
+            view.getLoginMessageLabel().setText("Could not log in");
+        }
     }
 }
